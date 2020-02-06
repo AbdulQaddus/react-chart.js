625a1a1882241f1da0dbb5956c639969c0c2071a
AHMAD BADSHA <abdulqaddus62@gmail.com>
2020-02-06 20:31:53 +0500

    added something new

---
+++ src/components/chart1.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chart from './components/Chart';
+export default class chart1 extends Component {
+  
+  render(){
+  return (
+    <div className="App">
+        <h1></h1>
+    </div>
+  );
+  }
+}
+
